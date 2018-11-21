4603d06c87a03573eb609e4f245f9e3a837e3a4a
ziekhart <andy_xz@live.com>
2018-11-21 19:31:56 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Countdown from './Countdown.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <br />
+
+        <h3 className="title">Cafilornia Bois, we almost there.</h3>
+        <h3 className="title">Jan 11, 2019.</h3>
+        <Countdown date="2019-01-11" />
+        <br />
+
+        <h2 className="Link">
+          edit the <a href="https://goo.gl/mqxmbf">doc</a>
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
