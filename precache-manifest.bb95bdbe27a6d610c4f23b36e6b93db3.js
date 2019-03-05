8e85e0b887311ad457172c320e28712b0a35cb27
Andrés A. Acevedo <andy_xz@live.com>
2019-03-05 10:38:09 -0400

    Updates

---
+++ precache-manifest.bb95bdbe27a6d610c4f23b36e6b93db3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60e9d058e3154f04736a",
+    "url": "/trip-countdown/static/js/runtime~main.60e9d058.js"
+  },
+  {
+    "revision": "9e2c0f6f283e2d261708",
+    "url": "/trip-countdown/static/js/main.9e2c0f6f.chunk.js"
+  },
+  {
+    "revision": "6ca6d44cc43d91638f34",
+    "url": "/trip-countdown/static/js/2.6ca6d44c.chunk.js"
+  },
+  {
+    "revision": "9e2c0f6f283e2d261708",
+    "url": "/trip-countdown/static/css/main.eae55605.chunk.css"
+  },
+  {
+    "revision": "b088ed606e8039d21a06e3251d3b95e3",
+    "url": "/trip-countdown/index.html"
+  }
+];
